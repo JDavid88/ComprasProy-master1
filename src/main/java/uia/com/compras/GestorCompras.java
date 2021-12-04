b7030634ca622f30d5cba5b99754561f835e972b
Your Name <you@example.com>
2021-12-04 09:07:37 -0600

    Initial commit

---
+++ src/main/java/uia/com/compras/GestorCompras.java
@@ -0,0 +1,109 @@
+package uia.com.compras;
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+
+/**
+ * @author amiguel
+ * @version 1.0
+ * @created 12-nov.-2019 11:27:37 a. m.
+ */
+public class GestorCompras {
+	private int opcion;
+    private ListaReportesNivelStock miReporteNS;
+    private PeticionOrdenCompra miPeticionOC = new PeticionOrdenCompra();
+    private SolicitudOrdenCompra miSolicituOC;
+    private Comprador miComprador = new Comprador();
+    private ArrayList<SolicitudOrdenCompra> misSolicitudesOC;
+    private HashMap<Integer, ArrayList<Cotizacion>> misSolicitudesCotizacionte;
+    //Aqui tienen que ir las otras
+
+    protected HashMap<Object, HashMap<Integer, ArrayList<Cotizacion>>> misSolicitudesCotizacion = new HashMap<Object, HashMap<Integer, ArrayList<Cotizacion>>>();
+
+
+    public GestorCompras() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        try {
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+			miReporteNS = mapper.readValue(new FileInputStream("F:\\IS\\examen\\ComprasProy-master\\arregloItemsV1.json"), ListaReportesNivelStock.class );
+            
+        }
+        catch (JsonParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        if (miReporteNS != null)
+        {
+            miPeticionOC.agregaItems(miReporteNS);
+
+            System.out.println("----- Items List -----");
+
+            for(int i=0; i<miReporteNS.getItems().size(); i++) {
+                InfoComprasUIA miNodo = miReporteNS.getItems().get(i);
+                miNodo.print();
+            }
+
+            miComprador.hazSolicitudOrdenCompra(miPeticionOC);
+        }
+
+        miSolicituOC=miComprador.buscaVendedor(miPeticionOC);
+        miComprador.agrupaVendedores(miSolicituOC);
+        int iOrden = 1;
+
+        if(misSolicitudesOC == null)
+            misSolicitudesOC = new ArrayList<SolicitudOrdenCompra>();
+
+        for (Entry<Integer, HashMap<Integer, ArrayList<InfoComprasUIA>>> item : miComprador.getSolicitudesOrdenCompraAgrupadosXvendedores().entrySet())
+        {
+            int iVendedor = item.getKey();
+            HashMap<Integer, ArrayList<InfoComprasUIA>> nodo = item.getValue();
+             //genero el identificador  idCompra
+            int idCompra = item.getKey()*1000+ iOrden*100;
+             //Formateando para ser un documento de SolicitudIrdenCompra por lo que creo una clase SolicitudOrdenCompra
+
+            //SolicitudOrdenCompra(@JsonProperty("id")int id, @JsonProperty("name")String name,
+            //@JsonProperty("codigo")String codigo, @JsonProperty("unidad")String unidad,
+            //@JsonProperty("cantidad")int cantidad, @JsonProperty("vendedor")int vendedor,@JsonProperty("clasificacionProveedor")int clasificacionVendedor)
+
+            for (Entry<Integer, ArrayList<InfoComprasUIA>> soc : nodo.entrySet())
+            {
+                SolicitudOrdenCompra newSolicitud = new SolicitudOrdenCompra(idCompra, "SOC-" + idCompra, "", "", 0, item.getKey(), soc.getKey());
+                newSolicitud.setItems(soc.getValue());
+                misSolicitudesOC.add(newSolicitud);
+                mapper.writeValue(new File("F:\\IS\\examen\\ComprasProy-master\\SolicitudOrdenCompra-" + newSolicitud.getName() + ".json"), newSolicitud);
+            }
+        }
+
+        //--Envio a Comprador las cotizacion para que genere al menos 3 cotizaciones con vendedores diferentes (0-4)
+        HashMap<Integer, ArrayList<Cotizacion>> misSolicitudesCotizacion =  miComprador.hazCotizaciones(misSolicitudesOC, mapper);
+
+        HashMap<Integer, Cotizacion> misSolicitudesOrdenCompra =  miComprador.seleccionaVendedores(misSolicitudesCotizacion, mapper);
+
+	}
+
+
+    public void print()
+    {
+    }
+
+}//end KardexListaKClientes
